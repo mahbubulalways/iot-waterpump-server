122e72d36fae6c1070f6f5f1e09794d56696dc58
mahbubulalways <mahbubulhasan604@gmail.com>
2026-04-13 14:19:45 +0600

    server ready

---
+++ dist/socket/socket.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getIO = exports.initSocket = void 0;
+const socket_io_1 = require("socket.io");
+const events_1 = require("./events");
+let io;
+const initSocket = (server) => {
+    io = new socket_io_1.Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+        },
+    });
+    io.on("connect", (socket) => {
+        console.log("🟢 User connected:", socket.id);
+        (0, events_1.socketEvents)(socket, io);
+        socket.on("disconnect", () => {
+            console.log("🔴 User disconnected:", socket.id);
+        });
+    });
+    return io;
+};
+exports.initSocket = initSocket;
+// optional: export io to use anywhere
+const getIO = () => {
+    if (!io)
+        throw new Error("Socket not initialized!");
+    return io;
+};
+exports.getIO = getIO;
